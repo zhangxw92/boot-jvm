b2cf09852cfc80058b5a8e9bc3af22e0e9a0492f
zhangxw03 <zhangxw_92@163.com>
2021-02-02 14:32:26 +0800

    task-00001:elasticsearch demo

--- src/main/java/com/athome/kafka/KafkaConsumer.java
+++ src/main/java/com/athome/kafka/KafkaConsumer.java
@@ -1,6 +1,7 @@
 package com.athome.kafka;
 
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
@@ -9,17 +10,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class KafkaConsumer {
 
-    /**
-     * 消息监听
-     */
-    @StreamListener(value = HelloMessageChannel.Hello)
-    public void receive(Message<String> message) {
-
-        System.out.println("------------------" + message.toString());
-
+    @StreamListener(HelloMessageChannel.Hello)
+    public void process(Message<?> message) {
         Acknowledgment acknowledgment = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
         if (acknowledgment != null) {
+            System.out.println("Acknowledgment provided");
             acknowledgment.acknowledge();
         }
     }
+
 }
+
+/**
+ * 消息监听
+ */
+//    @StreamListener(value = HelloMessageChannel.Hello)
+//    public void receive(Message<?> message) {
+//
+//        System.out.println("------------------" + message.toString());
+//
+//        Acknowledgment acknowledgment = message.getHeaders().get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
+//        if (acknowledgment != null) {
+//            acknowledgment.acknowledge();
+//        }
+//    }
+//}
